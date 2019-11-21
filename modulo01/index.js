1e6be44f0a3bf6202f695eab9e17dcd0be43b5c3
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-11-20 21:29:36 -0300

    método post e express.json

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,16 +2,30 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "name": "Kairo" }
 
 const users = ['Kairo', 'Cláudio', 'Victor']
 
+server.get('/users', (req, res) => {
+  return res.json(users);
+})
+
 server.get('/users/:index', (req, res) => {
   const { index } = req.params;
 
   return res.json(users[index]);
 });
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+})
 
 server.listen(3000);
