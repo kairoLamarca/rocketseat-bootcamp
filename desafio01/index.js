efabed22a0d06bb86ba336c98b05e1bc1261322d
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-11-21 19:20:30 -0300

    Criar tarefas no projeto

--- desafio01/index.js
+++ desafio01/index.js
@@ -6,20 +6,31 @@ server.use(express.json());
 
 //"prettier.singleQuote" : true
 
+
+
 const projects = [];
 
+// Criar projeto
 server.post('/projects', (req, res) => {
   const { id, title } = req.body;
 
-  projects.push({ id, title });
+  const project = {
+    id,
+    title,
+    tasks: []
+  }
+
+  projects.push(project);
 
   return res.json(projects);
 });
 
+// Lista todos os projetos
 server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
+// Altera um projeto por id
 server.put('/projects/:id', (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
@@ -31,6 +42,7 @@ server.put('/projects/:id', (req, res) => {
   return res.json(projects);
 });
 
+// Excluir projeto
 server.delete('/projects/:id', (req, res) => {
   const { id } = req.params;
 
@@ -39,6 +51,18 @@ server.delete('/projects/:id', (req, res) => {
   projects.splice(projectIndex, 1);
 
   return res.send();
+})
+
+// Incluir tarefas
+server.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.tasks.push(title);
+
+  return res.json(projects);
 })
 
 server.listen(3000);
